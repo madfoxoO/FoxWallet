281b57a0a59d0ee870e132814cc92fb56ae29922
Arseni Nikalayeu <madfoxoo@gmail.com>
2021-06-08 17:39:01 +0300

    FW1: Add brand colors and styles for bottom navigation

--- android/app/src/main/kotlin/com/github/madfoxoo/foxwallet/root/nav/NavigationView.kt
+++ android/app/src/main/kotlin/com/github/madfoxoo/foxwallet/root/nav/NavigationView.kt
@@ -19,29 +19,4 @@ class NavigationView @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyle: Int = 0
 ) : CoordinatorLayout(context, attrs, defStyle),
-    NavigationPresenter {
-
-    private lateinit var bottomAppBar: BottomAppBar
-    private lateinit var bottomNavigationView: BottomNavigationView
-
-    override fun onFinishInflate() {
-        super.onFinishInflate()
-        injectViews()
-        setupViews()
-    }
-
-    private fun injectViews() {
-        bottomAppBar = findViewById(R.id.bottomAppBar)
-        bottomNavigationView = findViewById(R.id.bottomNavigationView)
-    }
-
-    private fun setupViews() {
-        val radius = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8f, resources.displayMetrics)
-        val bottomAppBarDrawable = bottomAppBar.background as MaterialShapeDrawable
-        bottomAppBarDrawable.shapeAppearanceModel = bottomAppBarDrawable.shapeAppearanceModel
-            .toBuilder()
-            .setTopLeftCorner(CornerFamily.ROUNDED, radius)
-            .setTopRightCorner(CornerFamily.ROUNDED, radius)
-            .build()
-    }
-}
+    NavigationPresenter
